6eb0b8024009af527dcfd5398c52ba9d0c8f5bca
Rata0 <nekon689@gmail.com>
2025-03-09 15:30:44 +0300

    добавил реакт роуйтер + категории

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,29 @@
 import NavBar from './components/NavBar'
+import ScrollMenu from './components/ScrollMenu'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function PageOne() {
+  return (
+    <div>Page One</div>
+  )
+}
+
+function PageTwo() {
+  return (
+    <div>Page Two</div>
+  )
+}
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-    </div>
+      <ScrollMenu />
+      <Routes>
+        <Route path="home" element={<PageOne />} />
+        <Route path="products" element={<PageTwo />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { IoMenuSharp } from "react-icons/io5";
 import { IoIosClose } from "react-icons/io";
+import { CiShoppingCart, CiHeart, CiLogin  } from "react-icons/ci";
 import { useState } from "react";
 
 export default function NavBar() {
@@ -8,21 +9,30 @@ export default function NavBar() {
   const handle = () => setNav(!nav);
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-blue-400">
-      <h1 className="w-full text-3xl font-bold">Hello</h1>
-      <ul className="hidden md:flex">
-          <li className="p-4">Главная</li>
-          <li className="p-4">Контакты</li>
-          <li className="p-4">Товары</li>
-          <li className="p-4">Войти</li>
+    <div className="border flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#000]">
+
+      <ul className="w-full flex font-bold text-sm uppercase">
+        <li className="p-4 hover:text-red-500 transition-colors duration-200">мужское</li>
+        <li className="p-4 hover:text-red-500 transition-colors duration-200">женское</li>
+        <li className="p-4 hover:text-red-500 transition-colors duration-200">о бренде</li>
       </ul>
 
       <div onClick={handle} className="block md:hidden">
-        {!nav ? <IoIosClose size={30} /> : <IoMenuSharp size={25} />}
+        {!nav ? <IoMenuSharp size={25} /> : <IoIosClose size={30} />}
       </div>
 
-      <div className={!nav ? "fixed left-0 top-0 h-full w-[60%] border-r bg-white ease-in-out duration-500" : "fixed left-[-100%]"}>
+      <ul className="hidden md:flex">
+        <li className="p-4 hover:text-red-500 transition-colors duration-200"><CiHeart size={30}/></li>
+        <li className="p-4 hover:text-red-500 transition-colors duration-200"><CiLogin size={30}/></li>
+        <li className="p-4 hover:text-red-500 transition-colors duration-200"><CiShoppingCart size={30}/></li>
+      </ul>
+
+      <div className={nav ? "fixed left-0 top-0 h-full w-[60%] border-r bg-white ease-in-out duration-500" : "fixed left-[-100%]"}>
         <h1 className="w-full text-3xl font-bold m-4">Hello</h1>
+
+        <div onClick={handle} className="block md:hidden">
+          {!nav ? <IoMenuSharp size={25} /> : <IoIosClose size={30} />}
+        </div>
 
         <ul className="pt-25 uppercase m-4">
           <li className="p-4 border-b-1">Главная</li>
@@ -31,7 +41,7 @@ export default function NavBar() {
           <li className="p-4">Войти</li>
         </ul>
       </div>
-
+      
     </div>
   )
 }
